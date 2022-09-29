7ac2acb48f2f74de77288209020e6b1592bc64f2
Yubraj khadka <88958005+Yubrajkhadka@users.noreply.github.com>
2022-09-29 13:18:06 +0545

    Add files via upload
    In this project we can use the html css js and bootstrap for the front end and use php and mysql for the backend.

---
+++ aboutus.php
@@ -0,0 +1,47 @@
+<div class="container mb-5">
+		<div class="row">
+			<div class="col-sm-6 offset-sm-3 mt-4 mb-4">
+				<h2 class="text-center" style="color:#348485">Meet Our Team</h2>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card-box text-center shadow" style="background-color:#CBE6EE;">
+					<div class="user-pic">
+						<img src="../Images/ishwar.jpeg" class="img-fluid p-4 rounded" alt="User Pic">
+					</div>
+					<h5>Ishwar Khadka</h5>
+					<h6>Full Stack Developer</h6>
+					<p>Email:ishwarkhadka@kcc.edu.np</p>
+					<a href="mailto:ishwarkhadka@kcc.edu.np" class="clickableBtn btn mb-3">Contact</a>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card-box text-center shadow" style="background-color:#CBE6EE;">
+					<div class="user-pic">
+						<img src="../Images/prabhat.jpeg" class="img-fluid p-4 rounded" alt="User Pic">
+					</div>
+					<h5>Prabhat Kiran Sigdel</h5>
+					<h6>Database Developer</h6>
+					<p>Email:prabhatkiransigdel@kcc.edu.np</p>
+                    <a href="mailto:prabhatkiransigdel@kcc.edu.np" class="clickableBtn btn mb-3">Contact</a>
+
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card-box text-center shadow" style="background-color:#CBE6EE;">
+					<div class="user-pic">
+						<img src="../Images/yubraj.jpeg" class="img-fluid p-4 rounded" alt="User Pic">
+					</div>
+					<h5>Yubraj Khadka</h5>
+					<h6>Front End Developer</h6>
+					<p>Email:yubrajkhadka@kcc.edu.np</p>
+                    <a href="mailto:yubrajkhadka@kcc.edu.np" class="clickableBtn btn mb-3">Contact</a>
+
+				</div>
+			</div>
+		</div>
+	</div>
